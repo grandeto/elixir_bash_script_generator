50f7b9e3709f5996f521cf53b39af1aae702debd
grandeto <grandeto.com@gmail.com>
2020-01-26 01:03:40 +0200

    Implement tasks sorting logic

--- lib/elixir_bash_script_generator.ex
+++ lib/elixir_bash_script_generator.ex
@@ -3,7 +3,7 @@ defmodule ElixirBashScriptGenerator do
     Documentation for ElixirBashScriptGenerator.
     """
 
-    @first_task_name "task-1"
+    # @first_task_name "task-1"
 
     @doc """
     Elixir Bash Script Generator
@@ -14,9 +14,8 @@ defmodule ElixirBashScriptGenerator do
 
     """
     def generate(data) when is_list(data) do
-        %{"sorted" => sorted, "executed" => executed, "queued" => _} = sort_tasks(data)
-        IO.inspect(executed)
-        sorted
+        %{"sorted" => sorted, "executed" => executed, "queued" => _queued} = sort_tasks(data)
+        Enum.reverse(sorted)
     end
 
     def sort_tasks(tasks) do
@@ -26,64 +25,72 @@ defmodule ElixirBashScriptGenerator do
     end
 
     def handle_tasks_sorting(task, acc) do
-        if task["name"] === @first_task_name do
-            acc = add_to_acc(task, acc, "sorted")
-            add_to_acc(task, acc, "executed", "name")
+        if !task["requires"] do
+            task = Map.delete(task, "in_queue")
+            acc = Map.put(acc, "sorted", append_value_to_list_in_map(acc, "sorted", task))
+            acc = Map.put(acc, "executed", append_value_to_list_in_map(acc, "executed", task, "name"))
+            check_queued_on_task_executed(task, acc)
         else
-            handle_tail_tasks_sorting(task, acc)
+            handle_task_requires(task, acc)
         end
     end
 
-    def handle_tail_tasks_sorting(task, acc) do
-        if task["requires"] && (List.last(task["requires"]) in acc["executed"]) do
-            acc = add_to_acc(task, acc, "sorted")
-            add_to_acc(task, acc, "executed", "name")
-            handle_queued_tasks(acc, task["name"])
+    def check_queued_on_task_executed(task, acc) do
+        IO.inspect(acc)
+        if acc["queued"][task["name"]] do
+            queued = acc["queued"]
+            task_queued_jobs = Enum.reverse(queued[task["name"]])
+            queued = Map.delete(queued, task["name"])
+            acc = Map.put(acc, "queued", queued)
+            Enum.reduce(task_queued_jobs, acc, fn task_from_job, acc ->
+                handle_tasks_sorting(task_from_job, acc)
+            end)
         else
-            # TODO: add_to_queued_tasks
             acc
         end
     end
 
-    def handle_queued_tasks(acc, executed_task_name) do
-        %{
-            "sorted" => [],
-            "executed" => [],
-            "queued" => %{"task-5" => [
-                                        %{
-                                            "name" => "task-2",
-                                            "command" => "cat /tmp/file1",
-                                            "requires" => [
-                                                "task-5"
-                                            ]
-                                        },
-                                        %{
-                                            "name" => "task-3",
-                                            "command" => "cat /tmp/file1",
-                                            "requires" => [
-                                                "task-5"
-                                            ]
-                                        }
-                                    ]
-                        }
-        }
-        queue = acc["queued"]
-        if queue[executed_task_name] do
-            {task, left_queued_tasks} = List.pop_at(queue[executed_task_name], 0)
-            acc = add_to_acc(task, acc, "sorted")
-            acc = add_to_acc(task, acc, "executed", "name")
-            # TODO finish this
-            # TODO validate requires are sequenced
+    def handle_task_requires(task, acc) do
+        requires = task["requires"]
+        requires = Enum.reduce(requires, [], fn req_task_name, req_acc ->
+            if req_task_name in acc["executed"], do: req_acc, else: [req_task_name | req_acc]
+        end)
+        if Enum.empty?(requires) do
+            task = Map.delete(task, "requires")
+            handle_tasks_sorting(task, acc)
+        else
+            requires = should_be_queued(requires, task)
+            if Enum.empty?(requires), do: acc, else: handle_add_to_queue(task, acc, requires)
         end
     end
 
-    def append_task_to_list(task, map, key, task_key) do
-        temp_list = map[key]
-        value = if Kernel.is_nil(task_key), do: task, else: task[task_key]
-        List.insert_at(temp_list, -1, value)
+    def should_be_queued(requires, task) do
+        if task["in_queue"] do
+            Enum.reduce(requires, [], fn req_task_name, should_be_queued_acc ->
+                if req_task_name in task["in_queue"], do: should_be_queued_acc, else: [req_task_name | should_be_queued_acc]
+            end)
+        else
+            Map.put(task, "in_queue", requires)
+            requires
+        end
     end
 
-    def add_to_acc(task, acc, acc_key, task_key \\ nil) do
-        Map.put(acc, acc_key, append_task_to_list(task, acc, acc_key, task_key))
+    def handle_add_to_queue(task, acc, requires) do
+        Enum.reduce(requires, acc, fn req_task_name, acc ->
+          if acc["queued"][req_task_name] do
+            queued = append_value_to_list_in_map(acc["queued"], req_task_name, task)
+            Map.put(acc, "queued", queued)
+          else
+            queued = acc["queued"]
+            queued = Map.put(queued, req_task_name, [task])
+            Map.put(acc, "queued", queued)
+          end
+        end)
+    end
+
+    def append_value_to_list_in_map(map, key, value, value_key \\ nil) do
+        temp_list = map[key]
+        value = if Kernel.is_nil(value_key), do: value, else: value[value_key]
+        [value | temp_list]
     end
 end
