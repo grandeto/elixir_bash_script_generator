1ebe01feef98e8ef7effb5af243dc2130d4015b8
grandeto <grandeto.com@gmail.com>
2020-01-30 01:04:38 +0200

    Add sorting test

--- test/elixir_bash_script_generator_test.exs
+++ test/elixir_bash_script_generator_test.exs
@@ -2,7 +2,16 @@ defmodule ElixirBashScriptGeneratorTest do
   use ExUnit.Case
   doctest ElixirBashScriptGenerator
 
-  test "greets the world" do
-    assert ElixirBashScriptGenerator.hello() == :world
+  test "bash tasks are sorted correctly" do
+    sorted = Poison.decode!(File.read!("./bash_tasks/print_text_task_1.json"))
+    |> Map.get("tasks")
+    |> ElixirBashScriptGenerator.sort()
+
+    assert sorted === [
+        %{"command" => "touch file1", "name" => "task-1"},
+        %{"command" => "echo 'Hello World!' > file1", "name" => "task-3"},
+        %{"command" => "cat file1", "name" => "task-2"},
+        %{"command" => "rm file1", "name" => "task-4"}
+      ]
   end
 end
